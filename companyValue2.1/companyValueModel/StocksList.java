c75a1e02af85434eb4863b426b42e060d3d67a94
J-Daniel-S <59487880+J-Daniel-S@users.noreply.github.com>
2020-03-14 01:39:07 -0500

    Add files via upload
    Wherein Stock was finished (at least it seems to be. I'll know for sure as I build the ui).  The next step will be storing them in json.

---
+++ companyValue2.1/companyValueModel/StocksList.java
@@ -0,0 +1,37 @@
+package companyValueModel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class StocksList {
+
+	private List<Stock> stocks;
+
+	public StocksList() {
+		stocks = new ArrayList<Stock>();
+	}
+
+	public void addStock(String ticker) {
+		Stock stock = new Stock(ticker.toUpperCase());
+		stocks.add(stock);
+	}
+
+	public void addStock(HashMap<String, String> profile) {
+		Stock stock = new Stock(profile);
+		stocks.add(stock);
+	}
+
+	public List<Stock> getStocks() {
+		return stocks;
+	}
+
+	public Stock get(int index) {
+		return stocks.get(index);
+	}
+
+//	for testing
+	public void addStock(Stock stock) {
+		stocks.add(stock);
+	}
+}
